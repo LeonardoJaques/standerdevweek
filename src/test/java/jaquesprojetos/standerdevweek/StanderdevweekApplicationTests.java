d39bf0ed4fa797bae14c4e462d161853bc44825f
Leonardo Jaques <jaques.projetos@outlook.com>
2024-07-05 11:19:16 -0300

    Initial commit

---
+++ src/test/java/jaquesprojetos/standerdevweek/StanderdevweekApplicationTests.java
@@ -0,0 +1,13 @@
+package jaquesprojetos.standerdevweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanderdevweekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
